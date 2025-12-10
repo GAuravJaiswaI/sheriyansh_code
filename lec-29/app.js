cbc9f93b29990e7e877637c2c9dc8a92ca171162
GauravGupio <gaurav.jaiswal@mygupio.com>
2025-12-10 14:07:25 +0530

    app.js make

---
+++ lec-29/app.js
@@ -0,0 +1,8 @@
+// console.log("Hello World");
+// let age = parseInt(prompt("enter a no"))
+
+
+// let a = "a love you";
+// // console.log(a.slice(1,5));
+let name = "gaurav"
+console.log(`my name is ${name}`);
