0b21257de3a36fd298bd2f608d8396d837a8fb0b
Gaurav <gauravjaiswal0531@gmail.com>
2025-12-15 23:01:01 +0530

    lec 33

--- lec-33/app.js
+++ lec-33/app.js
@@ -1,9 +1,13 @@
-console.log("1")
-for(let i=1; i<=100; i++){
-    console.log("i");
-}
+// console.log("1")
+// for(let i=1; i<=100; i++){
+//     console.log("i");
+// }
 
-let i=0;
-while(i<32){
-    console.log(i);
+// let i=0;
+// while(i<32){
+//     console.log(i);
+// }
+
+for(let i=1; i<=10; i++){
+    console.log(`5 * ${i} = ${5*i}`);
 }
