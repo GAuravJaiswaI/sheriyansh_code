4856453f0bc281e1017ac025bf5c4d818cb857c8
GauravGupio <gaurav.jaiswal@mygupio.com>
2025-12-15 15:59:32 +0530

    sdfsd

--- lec-29/app.js
+++ lec-29/app.js
@@ -6,3 +6,27 @@
 // // console.log(a.slice(1,5));
 let name = "gaurav"
 console.log(`my name is ${name}`);
+
+let count  = 5;
+console.log(count);
+count++;
+// console.log(count);
+// count--;
+// console.log(count);
+// count += 10;
+// console.log(count);
+// count -= 10;
+// console.log(count);
+// count *= 10;
+// console.log(count);
+
+5=="5";
+
+5==="5";
+
+
+
+var hello = function(){
+    console.log("hello");
+}
+hello();
