fdbc03d2e74ac1bba227bd282ddca27c189ac175
Gaurav Jaiswal <gauravjaiswal0531@gmail.com>
2026-01-18 22:47:38 +0530

    no commit

---
+++ lec - 43/script.js
@@ -0,0 +1,12 @@
+// math.random()
+
+let a = Math.random();
+console.log(a);
+
+
+console.log(a * 100);
+
+let v = Math.floor(a * 100);
+console.log(v);// without decimal number
+
+//
--- lec-42/script.js
+++ lec-42/script.js
@@ -8,13 +8,13 @@
 // event listener
 
 var h = document.getElementById('h1'); // selection of an element
-console.log(h); 
+console.log(h);
 
 h.innerHTML = 'dom pdhlo guy'; // changing HTML
 
 h.style.color = 'red'; // changing CSS
 
-h.addEventListener('click', function(){ // event listener// when h1 is clicked, then the function will be called and the color of h1 will change to blue
+h.addEventListener('click', function () { // event listener// when h1 is clicked, then the function will be called and the color of h1 will change to blue
     h.style.color = 'blue';
     h.innerHTML = 'dom pdho bhai';
 });
@@ -22,8 +22,11 @@ h.addEventListener('click', function(){ // event listener// when h1 is clicked, 
 var b1 = document.getElementById('#btn');
 
 
-b1.addEventListener('click', function(){// event listener// when button is clicked, then the function will be called and the background color of button will change to blue and the text will change to clicked
+b1.addEventListener('click', function () {// event listener// when button is clicked, then the function will be called and the background color of button will change to blue and the text will change to clicked
     b1.style.backgroundColor = 'blue'; // changing CSS
     b1.innerHTML = 'clicked'; // changing HTML
     b1.style.fontSize = '100px'; // changing CSS
 });
+
+
+
