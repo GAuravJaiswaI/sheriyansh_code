07d2b0f43be2a669205c829ec4afdd81b94e121b
Gaurav <gauravjaiswal0531@gmail.com>
2026-04-15 09:25:38 +0530

    feat: Define square variable as undefined to prevent function call errors

--- Akshay saini/execution context/app.js
+++ Akshay saini/execution context/app.js
@@ -1,5 +1,10 @@
 var n = 2;
 
+
+//square = undefined;
+// when i assign to squre = undefined then it will give error because we are trying to call a function which is not defined.
+// and a when we assign memory to a fucunction it could no t be undefined , it will be a function object and it will be stored in memory and we can call it.
+
 function  square (num){
     var ans = num * num;
     return ans;
@@ -10,3 +15,5 @@ console.log(square2);
 
 square3 = square(4);
 console.log(square3);   
+
+
